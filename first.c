f119a8715603587d64030e2b8c454f21bebabb38
Sangyoon Lee <yhdltkddbs@gmail.com>
2024-03-27 19:39:03 +0900

    first commit

---
+++ first.c
@@ -0,0 +1,28 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main(void)
+//{
+//	int a;
+//	printf("숫자 하나를 입력하세요 : ");
+//	scanf("%d", &a);
+//
+//
+//	printf("입력한 숫자는 %d입니다. \n", a);
+//	return 0;
+//}
+#include <stdio.h>
+int main(void)
+{
+	/*for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}*/
+	for (int i = 0; i < 0; i++)
+		printf("?");
+
+	return 0;
+}
